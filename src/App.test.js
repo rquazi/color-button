a5eaff87a7abd97a9c198f749c9fd70f56eae03b
rquazi <rumana.quazi@csc.fi>
2022-09-22 14:37:08 +0300

    Added Test for color button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //find an element with a role button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  //expect the background color to red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+  //click button
+  fireEvent.click(colorButton);
+
+  //expect the button text to change to red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
 });
+
+test('button turns blue when clicked', () => {
+
+});
+
+test('check initial conditions', () => {
+  //check the button is enabled
+
+  //checkbox starts unchecked
+})
